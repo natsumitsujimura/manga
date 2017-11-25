77cfcc2448dc567847fd1edef983e9449eb95799
NatsumiTsujimura <nat.figtree@gmail.com>
2017-11-25 19:17:44 +0900

    thumbnail

--- folders.sh
+++ folders.sh
@@ -4,10 +4,17 @@ find . -maxdepth 1 -type d | sed -e 's/^\.\///' > .temp
 awk -f - .temp << EOS > folders.js
 BEGIN{
 	print "var folders = new Array();";
+	print "var thumbnail = new Array();";
 	count = 0;
 }
 !/^\.$/{
-	print "folders[" count++ "] = \"" \$0 "\";"; 
+        print "folders[" count "] = \"" \$0 "\";";
+
+        cmd = "ls \"" \$0 "/thumbnail\" | head -n 1";
+        cmd | getline t;
+        print "thumbnail[" count "] =\"" t "\";";
+
+        count++;
 }
 EOS
 rm .temp
--- make_index.sh
+++ make_index.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-./folders.sh
 find data -maxdepth 1 -type d | sed -e '/^data$/d' | while read file
 do
 echo ${file}
 ./pages.sh ${file}
 echo
 done
+./folders.sh
